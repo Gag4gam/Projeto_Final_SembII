93723f7a90cb4b6dfe8fffdfcd64352a01ec5e01
Gabriel Santos <gabriel__cortes@hotmail.com>
2024-10-04 10:51:23 -0300

    fix+++

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Projeto_Final_SembII.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Projeto_Final_SembII.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Final_SembII")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d92f7b34308a5155c92009172e1a28a874b46358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+53479c683df6b709d323a115b5f62116ad16a1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Final_SembII")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Final_SembII")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
