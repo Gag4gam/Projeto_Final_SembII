3704482f8c31e800d69cdd081ce260e11cee4004
Saulo Jorenti <saulojorenti@hotmail.com>
2024-10-04 14:01:13 -0300

    Chart Space
    - Added Chart Space in cards.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -17,30 +17,81 @@ namespace Projeto_Final_SembII
             pageBackgroundLayout.BackgroundColor = (Color)Application.Current.Resources["pageBackgroundColor"];
             
             ToggleAccelerometer();
+            ToggleGyroscope();
+            ToggleMagnetometer();
 
         }
 
         private void OnDetailsButtonClicked(object sender, EventArgs e)
         {
-            System.Console.WriteLine("Chegou");
+
             var button = sender as ImageButton;
             var parameter = button?.CommandParameter as string;
-            System.Console.WriteLine(parameter);
+            var space = 230;
+
             switch (parameter)
             {
                 case "Accelerometer":
                     DetailsAccelerometer.IsVisible = !DetailsAccelerometer.IsVisible;
-                    BoxAccelerometer.HeightRequest = DetailsAccelerometer.IsVisible ? 360 : 230;
+
+                    space = space + (DetailsAccelerometer.IsVisible ? 130 : 0) + (GraphicAccelerometer.IsVisible ? 130 : 0);
+
+                    BoxAccelerometer.HeightRequest = space;
+
                     break;
                 case "Gyroscope":
                     DetailsGyroscope.IsVisible = !DetailsGyroscope.IsVisible;
-                    BoxGyroscope.HeightRequest = DetailsGyroscope.IsVisible ? 360 : 230;
+
+                    space = space + (DetailsGyroscope.IsVisible ? 130 : 0) + (GraphicGyroscope.IsVisible ? 130 : 0);
+
+                    BoxGyroscope.HeightRequest = space;
+
                     break;
                 case "Magnetometer":
                     DetailsMagnetometer.IsVisible = !DetailsMagnetometer.IsVisible;
-                    BoxMagnetometer.HeightRequest = DetailsMagnetometer.IsVisible ? 360 : 230;
+
+                    space = space + (DetailsMagnetometer.IsVisible ? 130 : 0) + (GraphicMagnetometer.IsVisible ? 130 : 0);
+
+                    BoxMagnetometer.HeightRequest = space;
+
                     break;
                 
+            }
+        }
+
+        private void OnGraphicsButtonClicked(object sender, EventArgs e)
+        {
+
+            var button = sender as ImageButton;
+            var parameter = button?.CommandParameter as string;
+            var space = 230;
+
+            Console.WriteLine("Chegou");
+            Console.WriteLine(parameter);
+            switch (parameter)
+            {
+                case "Accelerometer":
+                    GraphicAccelerometer.IsVisible = !GraphicAccelerometer.IsVisible;
+
+                    space = space + (DetailsAccelerometer.IsVisible ? 130 : 0) + (GraphicAccelerometer.IsVisible ? 130 : 0);
+                    BoxAccelerometer.HeightRequest = space;
+
+                    break;
+                case "Gyroscope":
+                    GraphicGyroscope.IsVisible = !GraphicGyroscope.IsVisible;
+
+                    space = space + (DetailsGyroscope.IsVisible ? 130 : 0) + (GraphicGyroscope.IsVisible ? 130 : 0);
+
+                    BoxGyroscope.HeightRequest = space;
+                    break;
+                case "Magnetometer":
+                    GraphicMagnetometer.IsVisible = !GraphicMagnetometer.IsVisible;
+
+                    space = space + (DetailsMagnetometer.IsVisible ? 130 : 0) + (GraphicMagnetometer.IsVisible ? 130 : 0);
+
+                    BoxMagnetometer.HeightRequest = space;
+                    break;
+
             }
         }
 
@@ -81,12 +132,74 @@ namespace Projeto_Final_SembII
                 AccelerometerY.Text = $"Y: {y} m/s^2";
                 AccelerometerZ.Text = $"Z: {z} m/s^2";
             }
-            else
-            {
-                Console.WriteLine("Formato de dados inválido.");
-            }
             
         }
 
+        private void ToggleGyroscope()
+        {
+            if (Gyroscope.Default.IsSupported)
+            {
+                if (!Gyroscope.Default.IsMonitoring)
+                {
+                    // Turn on gyroscope
+                    Gyroscope.Default.ReadingChanged += Gyroscope_ReadingChanged;
+                    Gyroscope.Default.Start(SensorSpeed.UI);
+                }
+                else
+                {
+                    // Turn off gyroscope
+                    Gyroscope.Default.Stop();
+                    Gyroscope.Default.ReadingChanged -= Gyroscope_ReadingChanged;
+                }
+            }
+        }
+        private void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)
+        {
+            var match = Regex.Match(e.Reading.ToString(), @"X:\s*([-+]?[0-9]*,[0-9]+),\s*Y:\s*([-+]?[0-9]*,[0-9]+),\s*Z:\s*([-+]?[0-9]*,[0-9]+)");
+            if (match.Success)
+            {
+                // Convertendo os valores para double
+                double x = double.Parse(match.Groups[1].Value, CultureInfo.GetCultureInfo("pt-BR"));
+                double y = double.Parse(match.Groups[2].Value, CultureInfo.GetCultureInfo("pt-BR"));
+                double z = double.Parse(match.Groups[3].Value, CultureInfo.GetCultureInfo("pt-BR"));
+                // Exibindo os valores
+                GyroscopeX.Text = $"X: {x} rad/s";
+                GyroscopeY.Text = $"Y: {y} rad/s";
+                GyroscopeZ.Text = $"Z: {z} rad/s";
+            }
+        }
+        private void ToggleMagnetometer()
+        {
+            if (Magnetometer.Default.IsSupported)
+            {
+                if (!Magnetometer.Default.IsMonitoring)
+                {
+                    // Turn on magnetometer
+                    Magnetometer.Default.ReadingChanged += Magnetometer_ReadingChanged;
+                    Magnetometer.Default.Start(SensorSpeed.UI);
+                }
+                else
+                {
+                    // Turn off magnetometer
+                    Magnetometer.Default.Stop();
+                    Magnetometer.Default.ReadingChanged -= Magnetometer_ReadingChanged;
+                }
+            }
+        }
+        private void Magnetometer_ReadingChanged(object sender, MagnetometerChangedEventArgs e)
+        {
+            var match = Regex.Match(e.Reading.ToString(), @"X:\s*([-+]?[0-9]*,[0-9]+),\s*Y:\s*([-+]?[0-9]*,[0-9]+),\s*Z:\s*([-+]?[0-9]*,[0-9]+)");
+            if (match.Success)
+            {
+                // Convertendo os valores para double
+                double x = double.Parse(match.Groups[1].Value, CultureInfo.GetCultureInfo("pt-BR"));
+                double y = double.Parse(match.Groups[2].Value, CultureInfo.GetCultureInfo("pt-BR"));
+                double z = double.Parse(match.Groups[3].Value, CultureInfo.GetCultureInfo("pt-BR"));
+                // Exibindo os valores
+                MagnetometerX.Text = $"X: {x} µT";
+                MagnetometerY.Text = $"Y: {y} µT";
+                MagnetometerZ.Text = $"Z: {z} µT";
+            }
+        }
     }
 }
