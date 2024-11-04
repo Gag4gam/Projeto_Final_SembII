06be67d1343a9386a794112f4f9223d8345c1cc1
Saulo Jorenti <saulojorenti@hotmail.com>
2024-11-04 09:20:55 -0300

    Changed URL

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Maui.Controls;
-using System;
+﻿
 using System.Globalization;
-using LiveCharts;
 using System.Text.RegularExpressions;
-using System.Timers;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 
 
 namespace Projeto_Final_SembII
@@ -11,21 +13,79 @@ namespace Projeto_Final_SembII
 
     public partial class MainPage : ContentPage
     {
-
+        private readonly HttpClient _httpClient = new HttpClient();
+        private Timer? _timer;
         private int _gyroscopeCount = 0;
         private int _magnetometerCount = 0;
+
+        private double[] _accelerometersensor = { 0.0, 0.0, 0.0 };
+        private double[] _gyroscopesensor = { 0.0, 0.0, 0.0 };
+        private double[] _magnetometersensor = {0.0,0.0,0.0};
+
         public MainPage()
         {
             InitializeComponent();
-
+            
             pageBackgroundLayout.BackgroundColor = (Color)Application.Current.Resources["pageBackgroundColor"];
 
             ExecutarEmThread();
-
+            StartTimer();
 
         }
 
+        private void StartTimer()
+        {
+            _timer = new Timer(async (e) =>
+            {
+                await GetDataAsync();
+            }, null, 0, 500); // Chama PostDataAsync a cada 500ms
+        }
 
+        public async Task GetDataAsync()
+        {
+            var data = new
+            {
+                ax = _accelerometersensor[0],
+                ay = _accelerometersensor[1],
+                az = _accelerometersensor[2],
+
+                gx = _gyroscopesensor[0],
+                gy = _gyroscopesensor[1],
+                gz = _gyroscopesensor[2],
+
+                mx = _magnetometersensor[0],
+                my = _magnetometersensor[1],
+                mz = _magnetometersensor[2]
+            };
+            _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "e99b7b66314e4b7abdf478ad0263f785");
+            try
+            {
+                var queryParameters = $"ax={data.ax.ToString(CultureInfo.InvariantCulture)}&ay={data.ay.ToString(CultureInfo.InvariantCulture)}&az={data.az.ToString(CultureInfo.InvariantCulture)}&gx={data.gx.ToString(CultureInfo.InvariantCulture)}&gy={data.gy.ToString(CultureInfo.InvariantCulture)}&gz={data.gz.ToString(CultureInfo.InvariantCulture)}&mx={data.mx.ToString(CultureInfo.InvariantCulture)}&my={data.my.ToString(CultureInfo.InvariantCulture)}&mz={data.mz.ToString(CultureInfo.InvariantCulture)}";
+                string url = $"https://webapigerenciamento.azure-api.net/api/retValues?{queryParameters}";
+
+                var json = JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync(url,content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+
+                }
+                else
+                {
+                    Console.WriteLine($"Request failed with status code: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HttpRequestException: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+
+        }
         private void ExecutarEmThread()
         {
             var t1 = new Thread(() =>
@@ -159,7 +219,9 @@ namespace Projeto_Final_SembII
                 double x = double.Parse(match.Groups[1].Value, CultureInfo.GetCultureInfo("pt-BR"));
                 double y = double.Parse(match.Groups[2].Value, CultureInfo.GetCultureInfo("pt-BR"));
                 double z = double.Parse(match.Groups[3].Value, CultureInfo.GetCultureInfo("pt-BR"));
-
+                _accelerometersensor[0] = double.Parse(match.Groups[1].Value);
+                _accelerometersensor[1] = double.Parse(match.Groups[2].Value);
+                _accelerometersensor[2] = double.Parse(match.Groups[3].Value);
                 // Exibindo os valores
                 AccelerometerX.Text = $"X: {x} m/s^2";
                 AccelerometerY.Text = $"Y: {y} m/s^2";
@@ -205,6 +267,11 @@ namespace Projeto_Final_SembII
                     double y = double.Parse(match.Groups[2].Value, CultureInfo.GetCultureInfo("pt-BR"));
                     double z = double.Parse(match.Groups[3].Value, CultureInfo.GetCultureInfo("pt-BR"));
                     // Exibindo os valores
+                    _gyroscopesensor[0] = x;
+                    _gyroscopesensor[1] = y;
+                    _gyroscopesensor[2] = z;
+
+
                     GyroscopeX.Text = $"X: {x} rad/s";
                     GyroscopeY.Text = $"Y: {y} rad/s";
                     GyroscopeZ.Text = $"Z: {z} rad/s";
@@ -248,6 +315,11 @@ namespace Projeto_Final_SembII
                     double y = double.Parse(match.Groups[2].Value, CultureInfo.GetCultureInfo("pt-BR"));
                     double z = double.Parse(match.Groups[3].Value, CultureInfo.GetCultureInfo("pt-BR"));
                     // Exibindo os valores
+
+                    _magnetometersensor[0] = x;
+                    _magnetometersensor[1] = y;
+                    _magnetometersensor[2] = z;
+
                     MagnetometerX.Text = $"X: {x} µT";
                     MagnetometerY.Text = $"Y: {y} µT";
                     MagnetometerZ.Text = $"Z: {z} µT";
